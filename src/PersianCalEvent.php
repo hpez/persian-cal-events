5ef819754f153b34b544e036e1708315d10bae70
Hassan Pezeshk <hpez58@gmail.com>
2019-07-29 19:35:12 +0430

    Initial commit

---
+++ src/PersianCalEvent.php
@@ -0,0 +1,82 @@
+<?php namespace hpez\PersianCalEvent;
+
+use Carbon\Carbon;
+use Morilog\Jalali\CalendarUtils;
+
+/**
+*
+*  @author Hassan Pezeshk
+*/
+class PersianCalEvent
+{
+
+    /**
+     * @param int $y
+     * @param int $m
+     * @param int $d
+     * @return array
+     */
+    private static function crawl($y, $m, $d)
+    {
+        $handle = curl_init();
+
+        $url = "https://www.time.ir/fa/event/list/0/$y/$m/$d";
+
+        curl_setopt($handle, CURLOPT_URL, $url);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+        $output = curl_exec($handle);
+
+        curl_close($handle);
+
+        if (strpos($output, 'eventHoliday') !== false) {
+            $start = strpos($output, '</span>', strpos($output, 'eventHoliday'));
+            $start += 7;
+            $end = strpos($output, '<span', $start);
+            $cause = substr($output, $start, $end - $start);
+            $cause = str_replace("\n", '', $cause);
+            $cause = str_replace("\r", '', $cause);
+            $cause = str_replace('"', '', $cause);
+            $cause = trim($cause);
+            return ['is_holiday' => true, 'cause' => $cause];
+        }
+
+        return ['is_holiday' => false];
+    }
+
+    /**
+     * @param int $y
+     * @param int $m
+     * @param int $d
+     * @return array
+     */
+    public static function jalali($y, $m, $d)
+    {
+        $greg = CalendarUtils::toGregorian($y, $m, $d);
+        if (Carbon::create($greg[0], $greg[1], $greg[2])->dayOfWeek == 5)
+            return ['is_holiday' => true, 'cause' => 'جمعه'];
+
+        return self::crawl($y, $m ,$d);
+    }
+
+    /**
+     * @param int|Carbon $y
+     * @param int|null $m
+     * @param int|null $d
+     * @return array
+     */
+    public static function gregorian($y, $m = null, $d = null)
+    {
+        if ($m == null) {
+            $m = $y->month;
+            $d = $y->day;
+            $y = $y->year;
+        }
+
+        if (Carbon::create($y, $m, $d)->dayOfWeek == 5)
+            return ['is_holiday' => true, 'cause' => 'جمعه'];
+
+        $ymd = CalendarUtils::toJalali($y, $m, $d);
+        return self::crawl($ymd[0], $ymd[1], $ymd[2]);
+    }
+}
---
+++ tests/BasicTest.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace hpez\PersianCalEvent;
+
+use Carbon\Carbon;
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function testJalali()
+    {
+        $result = PersianCalEvent::jalali(1398, 1, 1);
+        $this->assertEquals($result['is_holiday'], true);
+
+        $result = PersianCalEvent::jalali(1398, 5, 7);
+        $this->assertEquals($result['is_holiday'], false);
+    }
+
+    public function testGregorian()
+    {
+        $result = PersianCalEvent::gregorian(2019, 8, 12);
+        $this->assertEquals($result['is_holiday'], true);
+
+        $result = PersianCalEvent::gregorian(2019, 8, 11);
+        $this->assertEquals($result['is_holiday'], false);
+
+        $result = PersianCalEvent::gregorian(new Carbon('2019-08-12'));
+        $this->assertEquals($result['is_holiday'], true);
+
+        $result = PersianCalEvent::gregorian(new Carbon('first day of August 2019'));
+        $this->assertEquals($result['is_holiday'], false);
+    }
+}
